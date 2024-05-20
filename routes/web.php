acd591f2fa04a468dfc7f8bcf5105dbd0fa6c4cf
qiffym <qiww.dev@gmail.com>
2024-05-20 15:16:03 +0700

    verify email

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,14 @@ Route::get('/dashboard', Controllers\DashboardController::class)->middleware(['a
 Route::get('stores', [Controllers\StoreController::class, 'index'])->name('stores.index');
 
 Route::middleware('auth')->group(function () {
+    Route::middleware('verified')->group(function () {
+        // Stores
+        Route::resource('stores', Controllers\StoreController::class)->except('index');
+    });
+
     Route::get('/profile', [Controllers\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Stores
-    Route::resource('stores', Controllers\StoreController::class)->except('index');
 });
 
 require __DIR__ . '/auth.php';
